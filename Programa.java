01e3dc1f170b8ab5ad449e69a5a260cf5bfffc66
Mario Batres <mario.batres.gt@gmail.com>
2022-05-31 13:25:57 -0600

    Creando funcionalidad de suma.

--- Programa.java
+++ Programa.java
@@ -1,4 +1,8 @@
 public class Programa {
+
+	public static int sumar(int a, int b) {
+		return a + b;
+	}
 	
 	public static void main (String [] args) {
 		System.out.println("Inicio de de nuestro programa.");
