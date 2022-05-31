8878c87dc600b6e5f1d9ccfb2e3a6ca48c13d9ca
Mario Batres <mario.batres.gt@gmail.com>
2022-05-31 13:31:11 -0600

    Agregando ejemplo de suma.

--- Programa.java
+++ Programa.java
@@ -6,5 +6,10 @@ public class Programa {
 	
 	public static void main (String [] args) {
 		System.out.println("Inicio de de nuestro programa.");
+
+		int c = sumar(2, 4);
+
+		System.out.println("La suma de 2 + 4 = " + c);
+
 	}
 }
